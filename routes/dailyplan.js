45ad479956b3f524e8db5df38f4f990d28f7ca66
danah olivetree <danah.olivetree@gmail.com>
2017-10-07 17:43:20 -0600

    wrote get route

--- routes/dailyplan.js
+++ routes/dailyplan.js
@@ -1,10 +1,44 @@
-var express = require('express');
-var router = express.Router();
-const knex = require('knex')
+var express = require('express')
+var router = express.Router()
+const knex = require('../knex')
+const boom = require('boom')
+const bcrypt = require('bcrypt')
+// var date
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', (req, res, next) => {
+
+  if (!req.body.date) {
+    // return
+    knex('dailyplans')
+      .select('*')
+      .then( (plans) => {
+        res.send(plans)
+      })catch( (err) => next(err))
+  }
+  else {
+    date = req.body.date
+      //might need to parse date
+    knex('dailyplan')
+      .select('*')
+      //.select('dailyplans.plan_date as date' 'dailyplans_events.event_time as time', 'dailyplans_events.event', 'lessons.title as lesson', 'lessons.location_url as lessonLink')
+      .where('date', date)
+    //  .innnerJoin('dailyplans_events')
+    //  .on(dailyplans_events.dailyplan_id = dailyplans.id)
+    // .innerJoin('lessons')
+    // .on('lessons.id' = 'dailyplans_events.lesson_id')
+      .then( (plan) => {
+        res.send(plan)
+      }).catch( (err) => next(err))
+  }
+
+
+})
+
+
+router.post('/', (res, req, next) => {
+  const {} = req.body
+
+})
+
 
 module.exports = router;
