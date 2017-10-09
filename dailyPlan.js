aaf890c4068e1c05bfe300e59142bc66e9f07be5
Gena Israel <gena.catharine@gmail.com>
2017-10-09 10:05:49 -0600

    testing git request

--- dailyPlan.js
+++ dailyPlan.js
@@ -5,20 +5,27 @@ $( document ).ready(function() {
 
 $(".chicken").datepicker({
 onSelect: function(dateText){
-  console.log(this.value)
+  // console.log('this value', this.value)
 
- $.get( "dailyPlan.js", function() {
-  alert( "success" );
+ $.get( "dailyPlan.js", {"date": this.value}, function(data) {
+data.forEach(function(item){
+  console.log(item)
+  // res.render('dailyplan')
+
 })
-  .done(function() {
-    alert( "second success" );
-  })
-  .fail(function() {
-    alert( "error" );
-  })
-  .always(function() {
-    alert( "finished" );
-  });
+
+
+  // alert( "success" );
+})
+  // .done(function() {
+  //   alert( "second success" );
+  // })
+  // .fail(function() {
+  //   alert( "error" );
+  // })
+  // .always(function() {
+  //   alert( "finished" );
+  // });
 }
 })
 
