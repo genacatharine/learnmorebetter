841807758a00a6aa826b6d1b0b80f4cf5ba90f4d
Gena Israel <gena.catharine@gmail.com>
2017-10-09 11:32:26 -0600

    changes to ejs

--- public/javascripts/dailyPlan.js
+++ public/javascripts/dailyPlan.js
@@ -8,8 +8,15 @@ onSelect: function(dateText){
   // console.log('this value', this.value)
 
  $.get( "dailyPlan.js", {"date": this.value}, function(data) {
+   console.log(data)
 data.forEach(function(item){
   console.log(item)
+  tbody.append($`<tr>
+    <td>${}</td>
+    <td></td>
+    <td></td>
+
+    </tr>`)
   // res.render('dailyplan')
 
 })
