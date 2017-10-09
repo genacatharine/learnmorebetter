a86a2e85ea450c2f103c9063eb562f8050e3cdcf
danah olivetree <danah.olivetree@gmail.com>
2017-10-09 17:27:50 -0600

    fixed merge erros

--- public/javascripts/dailyPlan.js
+++ public/javascripts/dailyPlan.js
@@ -1,30 +1,11 @@
 $( document ).ready(function() {
-<<<<<<< HEAD
-  // $( function() {
-  //   $( "#datepicker" ).datepicker();
-  // });
 
-// $(".chicken").datepicker({
-// onSelect: function(dateText){
-  // console.log('this value', this.value)
 
- $.getJSON('/api/v1/dailyplan', {date: '2017-10-02'}).done( (data) => {
-  let tbody = $('#dailyPlan tbody')
-=======
-//   $( function() {
-//     $( "#datepicker" ).datepicker();
-//   });
-$(document).on('click', '.chicken', function(e){
-  console.log(this.value)
-})
-// $("#chicken").datepicker({
-// onSelect: function(dateText){
-//   console.log('this value', this.value)
-// }
+
+
  $.getJSON('/api/v1/dailyplan', {"date": this.value}).then( (data) => {
   console.log('get dailyplan ', data)
-  let tbody = $('#dailyplan tbody')
->>>>>>> fa4282257513713869cdb5e0d9202203b6cb4bd4
+  let tbody = $('#dailyPlan tbody')
   data.forEach( (item) => {
   tbody.append($(`<tr>
     <td>${item.time}</td>
@@ -34,7 +15,7 @@ $(document).on('click', '.chicken', function(e){
   })
 })
 
-<<<<<<< HEAD
+
 
   // .done(function() {
   //   alert( "second success" );
@@ -47,9 +28,9 @@ $(document).on('click', '.chicken', function(e){
   // });
 // }
 })
-=======
-}
->>>>>>> fa4282257513713869cdb5e0d9202203b6cb4bd4
+
+
+
 
   // $("this").click(function(){
   //   console.log("helloworld")
