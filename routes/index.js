6336d91d14206d4c7f22c1893f2a0faff7cc2d33
Gena Israel <gena.catharine@gmail.com>
2017-10-09 13:49:31 -0600

    added partials

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dailyPlan', { title: 'Learn.More.Better.'});
+  res.render('./', { title: 'Learn.More.Better.', layout: 'layout.ejs'});
 });
 
 module.exports = router;
