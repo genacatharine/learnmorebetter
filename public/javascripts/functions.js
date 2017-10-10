f196b7504e0320fb140a27ccaff04fff948de40c
Gena Israel <gena.catharine@gmail.com>
2017-10-10 11:19:48 -0600

    changed back the functions.ejs

---
+++ public/javascripts/functions.js
@@ -0,0 +1,14 @@
+$( function() {
+  //make the date picker
+ $( "#datepicker" ).datepicker();
+
+ // $('td[data-handler="selectDay"]').click(function){
+ //   console.log(this.date)
+ // }
+
+} );
+
+//  //
+//  $(".chicken").datepicker({
+//
+// }
