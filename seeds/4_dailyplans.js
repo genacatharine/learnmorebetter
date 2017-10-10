4b9729caf5f06800bb809d883af022ae352ee60f
SJB <StevenJBurns@users.noreply.github.com>
2017-10-10 12:08:38 -0600

    New seed data for dailyplans table; seed data for assignments table

--- seeds/4_dailyplans.js
+++ seeds/4_dailyplans.js
@@ -4,11 +4,21 @@ exports.seed = (knex, Promise) => {
     .del()
     .then(() => {
       return knex("dailyplans").insert([
-        {id: 1, date: "2-Oct-2017", users_id: 1},
-        {id: 2, date: "3-Oct-2017", users_id: 2},
-        {id: 3, date: "4-Oct-2017", users_id: 3},
-        {id: 4, date: "5-Oct-2017", users_id: 2},
-        {id: 5, date: "6-Oct-2017", users_id: 1},
+        {id: 1, date: "18-Sep-2017", users_id: 1},
+        {id: 2, date: "19-Sep-2017", users_id: 2},
+        {id: 3, date: "20-Sep-2017", users_id: 3},
+        {id: 4, date: "21-Sep-2017", users_id: 2},
+        {id: 5, date: "22-Sep-2017", users_id: 1},
+        {id: 6, date: "25-Sep-2017", users_id: 2},
+        {id: 7, date: "26-Sep-2017", users_id: 3},
+        {id: 8, date: "27-Sep-2017", users_id: 2},
+        {id: 9, date: "28-Sep-2017", users_id: 1},
+        {id: 10, date: "29-Sep-2017", users_id: 3},
+        {id: 11, date: "2-Oct-2017", users_id: 1},
+        {id: 12, date: "3-Oct-2017", users_id: 2},
+        {id: 13, date: "4-Oct-2017", users_id: 3},
+        {id: 14, date: "5-Oct-2017", users_id: 2},
+        {id: 15, date: "6-Oct-2017", users_id: 1}
       ]);
     })
 };
--- seeds/7_assignments.js
+++ seeds/7_assignments.js
@@ -1,13 +1,161 @@
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+exports.seed = (knex, Promise) => {
+  return knex('assignments')
+    .del()
+    .then(() => {
+      return knex('assignments').insert([
+        {
+          id: 1,
+          title: "CLI Assessment",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 2,
+          title: "Reading Javascript Expressions",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 3,
+          title: "Writing Javascript Expressions",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 4,
+          title: "Semantic HTML",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 5,
+          title: "CSS Checkpoint"
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 6,
+          title: "Project Pixel Art",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 7,
+          title: "Galvanize Delivers",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 8,
+          title: "Vim Checkpoint",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 9,
+          title: "Accumulator",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 10,
+          title: "LoDash",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 11,
+          title: "AJAX Hero",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 12,
+          title: "Q1 Project Demo",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 13,
+          title: "Pet Shop",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 14,
+          title: "Galvanize Contacts",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 15,
+          title: "Galvanize Food Truck",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 16,
+          title: "Galvanize Pet Grooming",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 17,
+          title: "Galvanize Bookshelf",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 18,
+          title: "Server Side Testing",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        },
+        {
+          id: 19,
+          title: "Q2 Project Demo",
+          description: "none",
+          location_url: "www.assignment.com",
+          submit_url: "www.submit.com",
+          mastery_id: 0
+        }
       ]);
     });
 };
