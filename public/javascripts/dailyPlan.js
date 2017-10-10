7f44e3123ece46c631a4a6c9ca78c9b3e81ff58d
Gena Israel <gena.catharine@gmail.com>
2017-10-10 12:02:15 -0600

    event handler works

--- public/javascripts/dailyPlan.js
+++ public/javascripts/dailyPlan.js
@@ -1,32 +1,36 @@
 $( document ).ready(function() {
-//   $( function() {
-//     $( "#datepicker" ).datepicker();
-//   });
-// $(document).on('click', '.chicken', function(e){
-//   console.log(this.value)
-// })
-// $("#chicken").datepicker({
-// onSelect: function(dateText){
-//   console.log('this value', this.value)
-// }
- $.getJSON('/api/v1/dailyplan', {"date": this.value}).then( (data) => {
+
+console.log($('#datepicker').value);
+
+ $.getJSON('/api/v1/dailyplan', {"date": '2017-10-02'}).then( (data) => {
   console.log('get dailyplan ', data)
-  let tbody = $('#dailyplan tbody')
+  let tbody = $('#dailyPlan tbody')
+  
   data.forEach( (item) => {
-  console.log(item)
   tbody.append($(`<tr>
     <td>${item.time}</td>
     <td>${item.plan}</td>
     <td><a href="${item.lessonLink}">${item.lesson}</a></td>
-      <div class="btn-group" role="group" aria-label="Basic example">
-        <button type="button" class="btn btn-secondary"><a href="/dailyplan/edit">Edit</a></button>
-        <button type="button" class="btn btn-secondary deleteBtn" data-id="${student.id}">Delete</button>
-      </div>
     </tr>`))
   })
 })
 
-}
+
+
+  // .done(function() {
+  //   alert( "second success" );
+  // })
+  // .fail(function() {
+  //   alert( "error" );
+  // })
+  // .always(function() {
+  //   alert( "finished" );
+  // });
+// }
+})
+
+
+
 
   // $("this").click(function(){
   //   console.log("helloworld")
