79b5e0445c817041a8b4b1e00b646b53cb13dddc
danah olivetree <danah.olivetree@gmail.com>
2017-10-11 10:57:53 -0600

    assignments table in progress

--- public/javascripts/assts.js
+++ public/javascripts/assts.js
@@ -3,22 +3,15 @@ $( document ).ready(function() {
  $.getJSON('/api/v1/assts', {userId: 5}).then( (data) => {
   console.log('get method assts data ', data)
   let tbody = $('#assignments tbody')
-  let newdata = [{title, description, locationUrl, submitUrl}, {title, description, locationUrl, submitUrl}, {title, description, locationUrl, submitUrl}]
-  newdata.forEach( (item) => {
-  // tbody.append($(`<tr>
-  //   <td>${item.title}</td>
-  //   <td>${item.description}</td>
-  //   <td><a href="${item.locationUrl}">${item.title}</a></td>
-  //   <td><a href="${item.submitUrl}">Submit Here</a></td>
-  //
-  //   </tr>`))
-  tbody.append($(`<tr>
-    <td>${item.title}</td>
-    <td>${item.description}</td>
-    <td><a href="${item.locationUrl}">lesson link</a></td>
-    <td><a href="${item.submitUrl}">Submit Here</a></td>
-    </tr>`))
+  data.forEach( (item) => {
+    tbody.append($(`<tr>
+      <td><a href="${item.locationUrl}">${item.title}</a></td>
+      <td><a href="${item.lessonUrl}">${item.lesson}</a></td>
+      <td><a href="${item.submitUrl}">Submit Here</a></td>
+      <td>${item.grade}</td>
+
+      </tr>`))
+    })
   })
-})
 
 })
--- routes/api/assts.js
+++ routes/api/assts.js
@@ -6,17 +6,24 @@ const bcrypt = require('bcrypt')
 
 
 router.get('/', (req, res, next) => {
-  console.log('am i here? i want to be here. this is the assts.js api route')
-  let id = req.params.userId
-  console.log('id ', id)
-  let assts = {}
-  console.log(assts)
-  knex('users')
-    .where('users.id', id)
-    .innerJoin('users_assignments', 'users.id', 'users_assignments.user_id')
-    .innerJoin('assignments', 'users_assignments.assignment_id', 'assignments.id')
-    .select('assignments.title as title', 'assignments.description as description', 'assignments.location_url as locationUrl', 'assignments.submit_url as submitUrl')
-    .then( (assts) => {
+  console.log('this is the assts.js api route')
+  // let id = req.params.userId
+  let id = 5
+  // knex('users_assignments')
+  //   .where('users_assignments.user_id', id)
+  //   .innerJoin('assignments', 'users_assignments.assignment_id', 'assignments.id')
+  //   .select('assignments.title as title', 'assignments.location_url as locationUrl', 'assignments.submit_url as submitUrl', 'users_assignments.grade as grade')
+  //   .then( (assts) => {
+  //     console.log('this is a console log of assignments ', assts)
+  //     res.send(assts)
+  //   }).catch( (err) => next(err))
+  knex('assignments')
+    .select('assignments.title', 'assignments.location_url as locationUrl', 'assignments.submit_url as submitUrl', 'users_assignments.grade')
+    .where('users_assignments.user_id', id)
+    .innerJoin('users_assignments', 'users_assignments.assignment_id', 'assignments.id')
+    .innerJoin('lessons', 'assignments.lesson_id', 'lessons.id')
+
+    .then ( (assts) => {
       console.log('this is a console log of assignments ', assts)
       res.send(assts)
     }).catch( (err) => next(err))
