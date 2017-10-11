88cd2bba802fd3540db4ec1630aa0d22e190f6b9
danah olivetree <danah.olivetree@gmail.com>
2017-10-11 17:15:03 -0600

    made dash files no routes yet

---
+++ routes/api/dash.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var router = express.Router()
+const knex = require('../../knex')
+const boom = require('boom')
+const bcrypt = require('bcrypt')
+
+
+// router.get('/', (req, res, next) => {
+//   let id = req.query.userId
+//   knex('assignments')
+//     .select('assignments.title', 'assignments.location_url as locationUrl', 'assignments.submit_url as submitUrl', 'units.title as unit', 'units.location_url as unitUrl', 'users_assignments.grade')
+//     .innerJoin('units', 'assignments.unit_id', 'units.id')
+//     .innerJoin('users_assignments', 'users_assignments.assignment_id', 'assignments.id')
+//     .where('users_assignments.user_id', id)
+//     .then ( (assts) => {
+//       res.send(assts)
+//     }).catch( (err) => next(err))
+// })
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 let bcrypt = require('bcrypt');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  bcrypt.hash("learnmorebetter", 10)
-    .then(console.log, console.error);
-  res.render('./', { title: 'Learn.More.Better.', _layoutFile: 'layout.ejs'});
-});
+// router.get('/', function(req, res, next) {
+//   bcrypt.hash("learnmorebetter", 10)
+//     .then(console.log, console.error);
+//   res.render('./', { title: 'Learn.More.Better.', _layoutFile: 'layout.ejs'});
+// });
 
 module.exports = router;
