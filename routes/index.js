e6763716c53438aa40aecc31e2ce1e2e1dbf8b29
SJB <StevenJBurns@users.noreply.github.com>
2017-10-11 10:16:44 -0600

    Added hashed password for Gena

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+let bcrypt = require('bcrypt');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  bcrypt.hash("learnmorebetter", 10)
+    .then(console.log, console.error);
   res.render('./', { title: 'Learn.More.Better.', layout: 'layout.ejs'});
 });
 
