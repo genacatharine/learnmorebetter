00a23b38a9b38e72f054ad91ca090c8c242b8378
danah olivetree <danah.olivetree@gmail.com>
2017-10-11 16:03:21 -0600

    helps get route works

--- public/javascripts/helps.js
+++ public/javascripts/helps.js
@@ -3,15 +3,17 @@ $(document).ready(function() {
   $.getJSON('/api/v1/helps', {
     userId: 5
   }).then((data) => {
+    console.log('json get query for helps resulted in ', data)
     let tbody = $('#helps tbody')
     data.forEach((item) => {
       tbody.append($(`<tr>
         <td>${item.title}</td>
         <td></td>
         <td></td>
-        <td>><button id='remove' data-id='${item.id}'>Remove</button></td>
+        <td><button id='remove' data-id='${item.id}'>Remove</button></td>
         </tr>
         <tr>
+        <td></td>
         <td>${item.firstName} ${item.lastName}</td>
         <td>${item.email}</td>
         </tr>`))
--- routes/api/helps.js
+++ routes/api/helps.js
@@ -8,17 +8,53 @@ const bcrypt = require('bcrypt')
 router.get('/', (req, res, next) => {
   console.log('am i here? i want to be here. this is the helps.js api route')
   let id = req.query.userId
-  let assts = {}
-  knex('users')
-    .where('id', id)
-    .innerJoin('helps', 'users.id', "helps.user_id")
-    .innerJoin('assignments', 'helps.assignment_id', 'assignments.id')
-    .select('users.first_name as firstName', 'users.last_name as lastName', 'users.email', 'assignments.title', 'helps.id')
-    .groupBy('assignments.title')
+  console.log('helps req.query.userId ', req.query.userId)
+  let hell = []
+  let newHelp = {}
+
+
+
+  // knex('assignments')
+  //   .innerJoin('helps', 'assignments.id', 'helps.assignment_id')
+  //   .innerJoin('users', 'users.id', 'helps.user_id')
+  //   .select('assignments.title', 'users.first_name as firstName', 'users.last_name as lastName', 'users.email')
+  //   .where('helps.user_id', 5)
+
+
+  var subquery = knex.pluck('assignment_id').from('helps')
+    .whereIn('user_id', id)
+
+  knex('helps')
+  .select('assignments.title', 'users.first_name as firstName', 'users.last_name as lastName', 'users.email', 'helps.id')
+  .innerJoin('assignments', 'helps.assignment_id', 'assignments.id')
+  .innerJoin('users', 'helps.user_id', 'users.id')
+  .whereIn('assignment_id', subquery)
+  .whereNot('users.id', id)
+
+    //   assignments.forEach( (id) => {
+    //     knex('users')
+    //       .innerJoin('helps', 'helps.user_id', 'users.id')
+    //       // .innerJoin('assignments', 'helps.assignments_id', 'assignments.id')
+    //       .select('assignments.title', 'users.first_name as firstName', 'users.last_name as lastName', 'users.email', 'helps.id')
+    //       .then( (rows) => {
+    //         rows.forEach( (row) => {
+    //           newHelp.helpId = id,
+    //           newHelp.asst = title,
+    //           newHelp.users =
+    //         })
+    //       })
+    //   })
+    // })
+
+
     .then( (data) => {
+      console.log(data)
+
       res.send(data)
     }).catch( (err) => next(err))
 })
+
+// select a.title, u.first_name as firstName, u.last_name as lastName, u.email from assignments a inner join helps on (a.id=helps.assignment_id) inner join users u on (helps.user_id=u.id) where (helps.user_id=5);
 
 router.post('/:id', (req, res, next) => {
   console.log('posting route')
