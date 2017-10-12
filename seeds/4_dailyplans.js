729c6e117c65e9d058e5d83fe161e11bdd6c2bf4
danah olivetree <danah.olivetree@gmail.com>
2017-10-12 15:36:05 -0600

    working on dashboard

--- seeds/4_dailyplans.js
+++ seeds/4_dailyplans.js
@@ -18,7 +18,22 @@ exports.seed = (knex, Promise) => {
         {id: 12, date: "3-Oct-2017", users_id: 2},
         {id: 13, date: "4-Oct-2017", users_id: 3},
         {id: 14, date: "5-Oct-2017", users_id: 2},
-        {id: 15, date: "6-Oct-2017", users_id: 1}
+        {id: 15, date: "6-Oct-2017", users_id: 1},
+        {id: 16, date: "7-Oct-2017", users_id: 1},
+        {id: 17, date: "8-Oct-2017", users_id: 2},
+        {id: 18, date: "9-Oct-2017", users_id: 3},
+        {id: 19, date: "10-Oct-2017", users_id: 2},
+        {id: 20, date: "11-Oct-2017", users_id: 1},
+        {id: 21, date: "12-Oct-2017", users_id: 2},
+        {id: 22, date: "13-Oct-2017", users_id: 3},
+        {id: 23, date: "14-Oct-2017", users_id: 2},
+        {id: 24, date: "15-Oct-2017", users_id: 1},
+        {id: 25, date: "16-Oct-2017", users_id: 3},
+        {id: 26, date: "17-Oct-2017", users_id: 1},
+        {id: 27, date: "18-Oct-2017", users_id: 2},
+        {id: 28, date: "19-Oct-2017", users_id: 3},
+        {id: 29, date: "20-Oct-2017", users_id: 2},
+        {id: 30, date: "21-Oct-2017", users_id: 1}
       ]);
     })
 };
--- seeds/5_dailyplans_events.js
+++ seeds/5_dailyplans_events.js
@@ -7,25 +7,25 @@ exports.seed = (knex, Promise) => {
         {
           id: 1,
           dailyplan_id: 11,
-          event_time: "2-Oct-2017 09:00:00",
+          event_time: "09:00:00",
           plan: "Stand Up & Challenge Warm Up"
         },
         {
           id: 2,
           dailyplan_id: 11,
-          event_time: "2-Oct-2017 10:00:00",
+          event_time: "10:00:00",
           plan: "Lesson 1 : Promises"
         },
         {
           id: 3,
           dailyplan_id: 11,
-          event_time: "2-Oct-2017 12:00:00",
+          event_time: "12:00:00",
           plan: "Lunch"
         },
         {
           id: 4,
           dailyplan_id: 11,
-          event_time: "2-Oct-2017 16:40:00",
+          event_time: "16:40:00",
           plan: "Stand Down"
         }
       ]);
