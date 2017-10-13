755d25eb4a989f0d1b94a3a2418775ce7883f598
danah olivetree <danah.olivetree@gmail.com>
2017-10-13 09:11:47 -0600

    fixed logout hang

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -18,57 +18,62 @@ $(document).ready(() => {
  d = month + "/" + day + "/" + year;
 
 
-   $.getJSON('/api/v1/dailyplan', {
-     date: d
-   }).then((data) => {
-     let tbody = $('.dailyplanwidget tbody')
-     $(tbody).empty()
-     data.forEach((item) => {
-       if (!item.unit) {
-         item.unit=''
-         item.unitLink=''
-       }
-         tbody.append($(`<tr>
-         <td>${item.time}</td>
-         <td>${item.plan}</td>
-         <td><a href="${item.unitLink}">${item.unit}</a></td>
-         </tr>`))
-     })
-   })
-
-   $.getJSON('/api/v1/assts', {
-     userId: 5
-   }).then((data) => {
-     console.log('getting')
-     let tbody = $('.asstswidget tbody')
-     data.forEach((item) => {
-       if (!item.grade) {
-         tbody.append($(`<tr>
-         <td><a href="${item.locationUrl}">${item.title}</a></td>
-         <td><a href="${item.submitUrl}" class="btn btn-default">Submit</a></td>
-         <td><button name="button" type="button" data-id="${item.id}" class="helpMe btn btn-primary"> Add Help </button></td>
-         </tr>`))
-       }
-     })
-   })
-
-   $.getJSON('/api/v1/helps', {
-     userId: 5
-   }).then((data) => {
-     let tbody = $('.helpswidget tbody')
-     for (item in data) {
+ $.getJSON('/api/v1/dailyplan', {
+   date: d
+ }).then((data) => {
+   let tbody = $('.dailyplanwidget tbody')
+   $(tbody).empty()
+   data.forEach((item) => {
+     if (!item.unit) {
+       item.unit=''
+       item.unitLink=''
+     }
        tbody.append($(`<tr>
-         <td>${item}</td>
-         </tr>`))
-         data[item].forEach( (user) => {
-           tbody.append($(`<tr>
-           <td></td>
-           <td>${user.first} ${user.last}</td>
-           <td>${user.email}</td>
-           </tr>`))
-         })
+       <td>${item.time}</td>
+       <td>${item.plan}</td>
+       <td><a href="${item.unitLink}">${item.unit}</a></td>
+       </tr>`))
+   })
+ })
+
+ $.getJSON('/api/v1/assts', {
+   userId: req.cookies.token.userId
+ }).then((data) => {
+   console.log('getting assignments widget')
+   console.log('userId was ', userId)
+   console.log('data from get ', data)
+   let tbody = $('.asstswidget tbody')
+   data.forEach((item) => {
+     if (!item.grade) {
+       tbody.append($(`<tr>
+       <td><a href="${item.locationUrl}">${item.title}</a></td>
+       <td><a href="${item.submitUrl}" class="btn btn-default">Submit</a></td>
+       <td><button name="button" type="button" data-id="${item.id}" class="helpMe btn btn-primary"> Add Help </button></td>
+       </tr>`))
      }
    })
+ })
+
+ $.getJSON('/api/v1/helps', {
+   userId: req.cookies.token.userId
+ }).then((data) => {
+   console.log('getting helps widget')
+   console.log('userId was ', userId)
+   console.log('data from get ', data)
+   let tbody = $('.helpswidget tbody')
+   for (item in data) {
+     tbody.append($(`<tr>
+       <td>${item}</td>
+       </tr>`))
+       data[item].forEach( (user) => {
+         tbody.append($(`<tr>
+         <td></td>
+         <td>${user.first} ${user.last}</td>
+         <td>${user.email}</td>
+         </tr>`))
+       })
+   }
+ })
 
 
 })
