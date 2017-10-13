b1ae42839471ff91283e51697520ced6055b9ec6
danah olivetree <danah.olivetree@gmail.com>
2017-10-13 09:23:49 -0600

    changed assts seeds to user 7

--- routes/assts.js
+++ routes/assts.js
@@ -3,6 +3,8 @@ var router = express.Router()
 const knex = require('../knex')
 const boom = require('boom')
 const bcrypt = require('bcrypt')
+const SECRET = process.env.JWT_KEY
+const jwt = require('jsonwebtoken')
 // var date
 
 router.get('/', function(req, res, next) {
