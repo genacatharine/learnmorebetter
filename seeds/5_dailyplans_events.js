42212a0d128eb1232d6f68211bfb92746100f442
Gena Catharine <gena.catharine@gmail.com>
2017-10-13 09:17:37 -0600

    changed seeds

--- seeds/5_dailyplans_events.js
+++ seeds/5_dailyplans_events.js
@@ -28,78 +28,78 @@ exports.seed = (knex, Promise) => {
           dailyplan_id: 22,
           event_time: "16:40:00",
           plan: "Stand Down"
-        }
+        },
         {
-          id: 1,
+          id: 5,
           dailyplan_id: 23,
           event_time: "09:00:00",
           plan: "Stand Up & Challenge Warm Up"
         },
         {
-          id: 2,
+          id: 6,
           dailyplan_id: 23,
           event_time: "10:00:00",
           plan: "Lesson : Promises",
           unit_id: 15
         },
         {
-          id: 3,
+          id: 7,
           dailyplan_id: 23,
           event_time: "12:00:00",
           plan: "Lunch"
         },
         {
-          id: 4,
+          id: 8,
           dailyplan_id: 23,
           event_time: "16:40:00",
           plan: "Stand Down"
-        }
+        },
         {
-          id: 1,
+          id: 9,
           dailyplan_id: 24,
           event_time: "09:00:00",
           plan: "Stand Up & Challenge Warm Up"
         },
         {
-          id: 2,
+          id: 10,
           dailyplan_id: 24,
           event_time: "10:00:00",
           plan: "Lesson : Promises",
           unit_id: 15
         },
         {
-          id: 3,
+          id: 11,
           dailyplan_id: 24,
           event_time: "12:00:00",
           plan: "Lunch"
         },
         {
-          id: 4,
+          id: 12,
           dailyplan_id: 24,
           event_time: "16:40:00",
           plan: "Stand Down"
-        }
+        },
         {
-          id: 1,
+          id: 13,
           dailyplan_id: 25,
           event_time: "09:00:00",
           plan: "Stand Up & Challenge Warm Up"
         },
         {
-          id: 2,
+          id: 14,
           dailyplan_id: 25,
           event_time: "10:00:00",
           plan: "Lesson : Promises",
           unit_id: 15
         },
         {
-          id: 3,
+          id: 15,
           dailyplan_id: 25,
           event_time: "12:00:00",
           plan: "Lunch"
         },
         {
-          id: 4,
+          id: 16,
           dailyplan_id: 25,
           event_time: "16:40:00",
           plan: "Stand Down"
