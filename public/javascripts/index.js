c0807c762c4bf808cba8e6f557446da6c40da147
danah olivetree <danah.olivetree@gmail.com>
2017-10-17 02:03:50 -0600

    all buttons working

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,88 +1,76 @@
 $(document).ready(() => {
 
- // $(".login").click(() => {
- //    let loginData = JSON.stringify({
- //      email: $("#loginEmail").val(),
- //      password: $("#loginPassword").val()
- //    });
- //
- //    console.log("Sending Data : ", loginData);
- //
- //    $.post("/", loginData)
- //      .done((response) => console.log(response), "json")
- //  });
- var d = new Date();
- var month = d.getUTCMonth() + 1; //months from 1-12
- var day = d.getUTCDate();
- var year = d.getUTCFullYear();
- d = month + "/" + day + "/" + year;
+  // $(".login").click(() => {
+  //    let loginData = JSON.stringify({
+  //      email: $("#loginEmail").val(),
+  //      password: $("#loginPassword").val()
+  //    });
+  //
+  //    console.log("Sending Data : ", loginData);
+  //
+  //    $.post("/", loginData)
+  //      .done((response) => console.log(response), "json")
+  //  });
+  var d = new Date();
+  var month = d.getUTCMonth() + 1; //months from 1-12
+  var day = d.getUTCDate();
+  var year = d.getUTCFullYear();
+  d = month + "/" + day + "/" + year;
 
 
- $.getJSON('/api/v1/dailyplan', {
-   date: d
- }).then((data) => {
-   let tbody = $('.dailyplanwidget tbody')
-   $(tbody).empty()
-   data.forEach((item) => {
-     if (!item.unit) {
-       item.unit=''
-       item.unitLink=''
-     }
-       tbody.append($(`<tr>
+  $.getJSON('/api/v1/dailyplan', {
+    date: d
+  }).then((data) => {
+    let tbody = $('.dailyplanwidget tbody')
+    $(tbody).empty()
+    data.forEach((item) => {
+      if (!item.unit) {
+        item.unit = ''
+        item.unitLink = ''
+      }
+      tbody.append($(`<tr>
        <td>${item.time}</td>
        <td>${item.plan}</td>
        <td><a href="${item.unitLink}">${item.unit}</a></td>
        </tr>`))
-   })
- })
+    })
+  })
 
- $.getJSON('/api/v1/assts').then((data) => {
-   console.log('getting assignments widget')
-
-   console.log('data from get assignments widget', data)
-   let tbody = $('.asstswidget tbody')
-   data.forEach((item) => {
-     if (!item.grade) {
-       tbody.append($(`<tr>
+  $.getJSON('/api/v1/assts').then((data) => {
+    let tbody = $('.asstswidget tbody')
+    data.forEach((item) => {
+      if (!item.grade) {
+        tbody.append($(`<tr>
        <td><a href="${item.locationUrl}">${item.title}</a></td>
-       <td><a href="${item.submitUrl}" class="btn btn-default">Submit</a></td>
+       <td><a href="${item.submitUrl}" class="btn btn-default" data-asst='${item}'>Submit</a></td>
        <td><button name="button" type="button" data-id="${item.id}" class="helpMe btn btn-primary"> Add Help </button></td>
        </tr>`))
-     }
-   })
- })
+        $('.helpMe').click((e) => {
+          let id = $(e.target).data('id')
+          $.post(`./helps/${id}`)
+            .done((data) => {
+            })
+        })
 
- $.getJSON('/api/v1/helps').then((data) => {
-   console.log('getting helps widget')
+      }
+    })
+  })
 
-   console.log('data from get helps', data)
-  //  let tbody = $('.helpswidget tbody')
-  //  for (item in data) {
-  //    tbody.append($(`<tr>
-  //      <td>${item}</td>
-  //      </tr>`))
-  //      data[item].forEach( (user) => {
-  //        tbody.append($(`<tr>
-  //        <td></td>
-  //        <td>${user.first} ${user.last}</td>
-   //
-  //        </tr>`))
-  //      })
-  //  }
-  let dash = $('.helpswidget tbody')
-  for (item in data) {
-    dash.append($(`<tr>
+  $.getJSON('/api/v1/helps').then((data) => {
+    let dash = $('.helpswidget tbody')
+    for (item in data) {
+      dash.append($(`<tr>
       <td>${item}</td>
       </tr>`))
-      data[item].forEach( (user) => {
+      data[item].forEach((user) => {
         dash.append($(`<tr>
         <td></td>
         <td> <a href="mailto:${user.email}">${user.first} ${user.last}</td>
 
         </tr>`))
       })
-  }
- })
+    }
+  })
 
 
 })
--- seeds/5_dailyplans_events.js
+++ seeds/5_dailyplans_events.js
@@ -6,101 +6,101 @@ exports.seed = (knex, Promise) => {
       return knex("dailyplans_events").insert([
         {
           id: 1,
-          dailyplan_id: 22,
+          dailyplan_id: 26,
           event_time: "09:00:00",
           plan: "Stand Up & Challenge Warm Up"
         },
         {
           id: 2,
-          dailyplan_id: 22,
+          dailyplan_id: 26,
           event_time: "10:00:00",
           plan: "Lesson : Promises",
           unit_id: 15
         },
         {
           id: 3,
-          dailyplan_id: 22,
+          dailyplan_id: 26,
           event_time: "12:00:00",
           plan: "Lunch"
         },
         {
           id: 4,
-          dailyplan_id: 22,
+          dailyplan_id: 26,
           event_time: "16:40:00",
           plan: "Stand Down"
         },
         {
           id: 5,
-          dailyplan_id: 23,
+          dailyplan_id: 27,
           event_time: "09:00:00",
           plan: "Stand Up & Challenge Warm Up"
         },
         {
           id: 6,
-          dailyplan_id: 23,
+          dailyplan_id: 27,
           event_time: "10:00:00",
           plan: "Lesson : Promises",
           unit_id: 15
         },
         {
           id: 7,
-          dailyplan_id: 23,
+          dailyplan_id: 27,
           event_time: "12:00:00",
           plan: "Lunch"
         },
         {
           id: 8,
-          dailyplan_id: 23,
+          dailyplan_id: 27,
           event_time: "16:40:00",
           plan: "Stand Down"
         },
         {
           id: 9,
-          dailyplan_id: 24,
+          dailyplan_id: 28,
           event_time: "09:00:00",
           plan: "Stand Up & Challenge Warm Up"
         },
         {
           id: 10,
-          dailyplan_id: 24,
+          dailyplan_id: 28,
           event_time: "10:00:00",
           plan: "Lesson : Promises",
           unit_id: 15
         },
         {
           id: 11,
-          dailyplan_id: 24,
+          dailyplan_id: 28,
           event_time: "12:00:00",
           plan: "Lunch"
         },
         {
           id: 12,
-          dailyplan_id: 24,
+          dailyplan_id: 28,
           event_time: "16:40:00",
           plan: "Stand Down"
         },
         {
           id: 13,
-          dailyplan_id: 25,
+          dailyplan_id: 29,
           event_time: "09:00:00",
           plan: "Stand Up & Challenge Warm Up"
         },
         {
           id: 14,
-          dailyplan_id: 25,
+          dailyplan_id: 29,
           event_time: "10:00:00",
           plan: "Lesson : Promises",
           unit_id: 15
         },
         {
           id: 15,
-          dailyplan_id: 25,
+          dailyplan_id: 29,
           event_time: "12:00:00",
           plan: "Lunch"
         },
         {
           id: 16,
-          dailyplan_id: 25,
+          dailyplan_id: 29,
           event_time: "16:40:00",
           plan: "Stand Down"
         }
