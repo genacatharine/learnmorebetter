fc8910a35aeff09a9c81487833e722595ea30817
danah olivetree <danah.olivetree@gmail.com>
2017-10-23 13:04:54 -0600

    fixed daily seeds

--- seeds/05_dailyplans_events.js
+++ seeds/05_dailyplans_events.js
@@ -6,101 +6,101 @@ exports.seed = (knex, Promise) => {
       return knex("dailyplans_events").insert([
         {
           id: 1,
-          dailyplan_id: 26,
+          dailyplan_id: 1,
           event_time: "09:00:00",
           plan: "Stand Up & Challenge Warm Up"
         },
         {
           id: 2,
-          dailyplan_id: 26,
+          dailyplan_id: 1,
           event_time: "10:00:00",
           plan: "Lesson : Promises",
           unit_id: 15
         },
         {
           id: 3,
-          dailyplan_id: 26,
+          dailyplan_id: 1,
           event_time: "12:00:00",
           plan: "Lunch"
         },
         {
           id: 4,
-          dailyplan_id: 26,
+          dailyplan_id: 1,
           event_time: "16:40:00",
           plan: "Stand Down"
         },
         {
           id: 5,
-          dailyplan_id: 27,
+          dailyplan_id: 2,
           event_time: "09:00:00",
           plan: "Stand Up & Challenge Warm Up"
         },
         {
           id: 6,
-          dailyplan_id: 27,
+          dailyplan_id: 2,
           event_time: "10:00:00",
           plan: "Lesson : Promises",
           unit_id: 15
         },
         {
           id: 7,
-          dailyplan_id: 27,
+          dailyplan_id: 2,
           event_time: "12:00:00",
           plan: "Lunch"
         },
         {
           id: 8,
-          dailyplan_id: 27,
+          dailyplan_id: 2,
           event_time: "16:40:00",
           plan: "Stand Down"
         },
         {
           id: 9,
-          dailyplan_id: 28,
+          dailyplan_id: 3,
           event_time: "09:00:00",
           plan: "Stand Up & Challenge Warm Up"
         },
         {
           id: 10,
-          dailyplan_id: 28,
+          dailyplan_id: 3,
           event_time: "10:00:00",
           plan: "Lesson : Promises",
           unit_id: 15
         },
         {
           id: 11,
-          dailyplan_id: 28,
+          dailyplan_id: 3,
           event_time: "12:00:00",
           plan: "Lunch"
         },
         {
           id: 12,
-          dailyplan_id: 28,
+          dailyplan_id: 3,
           event_time: "16:40:00",
           plan: "Stand Down"
         },
         {
           id: 13,
-          dailyplan_id: 29,
+          dailyplan_id: 4,
           event_time: "09:00:00",
           plan: "Stand Up & Challenge Warm Up"
         },
         {
           id: 14,
-          dailyplan_id: 29,
+          dailyplan_id: 4,
           event_time: "10:00:00",
           plan: "Lesson : Promises",
           unit_id: 15
         },
         {
           id: 15,
-          dailyplan_id: 29,
+          dailyplan_id: 4,
           event_time: "12:00:00",
           plan: "Lunch"
         },
         {
           id: 16,
-          dailyplan_id: 29,
+          dailyplan_id: 4,
           event_time: "16:40:00",
           plan: "Stand Down"
         }
