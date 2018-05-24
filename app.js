3f8fb005086c2d550af0f255290baa14d27b9e06
SJB <StevenJBurns@users.noreply.github.com>
2018-05-24 02:25:34 -0600

    Cleaned up app.js -- indents, semicolons and comments for consistency

--- app.js
+++ app.js
@@ -1,70 +1,71 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
-var express = require('express')
-var path = require('path')
-//var favicon = require('serve-favicon')
-var logger = require('morgan')
-var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
 
-var app = express()
+/* dependencies */
+var path = require('path');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+//var favicon = require('serve-favicon');
+var logger = require('morgan');
 
-var index = require('./routes/index')
-let login = require('./routes/login')
-let register = require('./routes/register')
-var dailyplan = require('./routes/dailyplan')
-var assts = require('./routes/assts')
-var helps = require('./routes/helps')
-var apidp = require('./routes/api/dailyplan')
-var apiassts = require('./routes/api/assts')
-var users = require('./routes/users');
-var apihelps = require('./routes/api/helps')
-var logout = require('./routes/logout')
+var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+/* routing modules */
+let index = require('./routes/index');
+let login = require('./routes/login');
+let register = require('./routes/register');
+let dailyplan = require('./routes/dailyplan');
+let assts = require('./routes/assts');
+let helps = require('./routes/helps');
+let apidp = require('./routes/api/dailyplan');
+let apiassts = require('./routes/api/assts');
+let users = require('./routes/users');
+let apihelps = require('./routes/api/helps');
+let logout = require('./routes/logout');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+/* view engine setup for EJS */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use('/', index)
-app.use('/login', login)
-app.use('/register', register)
-app.use('/users', users)
-app.use('/dailyplan', dailyplan)
-app.use('/assts', assts)
-app.use('/helps', helps)
-app.use('/api/v1/dailyplan', apidp)
-app.use('/api/v1/assts', apiassts)
-app.use('/api/v1/helps', apihelps)
-app.use('/logout', logout)
+/* uncomment after placing your favicon in /public */
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
+app.use('/', index);
+app.use('/login', login);
+app.use('/register', register);
+app.use('/users', users);
+app.use('/dailyplan', dailyplan);
+app.use('/assts', assts);
+app.use('/helps', helps);
+app.use('/api/v1/dailyplan', apidp);
+app.use('/api/v1/assts', apiassts);
+app.use('/api/v1/helps', apihelps);
+app.use('/logout', logout);
+
+/* catch 404 and forward to error handler */
 app.use(function(req, res, next) {
-  var err = new Error('Not Found')
-  err.status = 404
-  next(err)
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 })
 
-// error handler
+/* error handler */
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message
+  res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500)
+  res.status(err.status || 500);
   console.log(err.message);
-  res.render('error')
+  res.render('error');
 })
-
-
 
 module.exports = app
