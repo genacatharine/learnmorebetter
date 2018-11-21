8126bb99862f5c83cfad25d37cfa3eca46447f38
SJB <StevenJBurns@users.noreply.github.com>
2018-11-20 23:48:15 -0700

    Clean up in /routes/dailyplan.js

--- routes/dailyplan.js
+++ routes/dailyplan.js
@@ -1,19 +1,13 @@
-var express = require('express')
-var router = express.Router()
-const knex = require('../knex')
-const boom = require('boom')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const SECRET = process.env.JWT_KEY
+const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.JWT_KEY;
 
-router.get('/', function(req, res, next) {
-      jwt.verify(req.cookies.token, SECRET, (err, payload) => {
-        if (!req.cookies.token) {
-          res.redirect("./login")
-        } else {
-          res.render('./dailyplan/')
-        }
-      })
-})
+
+router.get('/', (req, res, next) => {
+  jwt.verify(req.cookies.token, SECRET, (err, payload) => {
+    (!req.cookies.token) ? res.redirect("./login") : res.render('./dailyplan/')
+  });
+});
+
 
 module.exports = router;
